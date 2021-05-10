369ab4e1b52306630c61a5f503f8667ab2cbcf57
Jordy1311 <jordancooper1195@gmail.com>
2021-05-11 08:53:39 +1200

    loading sample data into state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     state = {
@@ -17,6 +18,9 @@ class App extends React.Component {
         this.setState({
             fishes // same thing as fishes: fishes - can also be on one line
         });
+    };
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
     }
     render() {
         return (
@@ -25,7 +29,7 @@ class App extends React.Component {
                     <Header tagline='Fresh Seafood Market'/>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
     }
